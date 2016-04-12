bfdf15f3577dba314891b5b3b85e92e9b4e7566f
hapekituarangi <hape.porter@gmail.com>
2016-04-12 12:41:02 +1200

    Added code for exercise 1 solution

--- eloquent.js
+++ eloquent.js
@@ -1,1 +1,13 @@
+//Exercise 1 - Minimum
 
+var min = function(low, high) {
+if (low < high)
+  return low;
+else
+  return high;
+}
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
